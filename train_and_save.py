d9ee2d0a6c944b78fdc0b0b7cd6922e231388a1c
MakhmoodSodikov <sodikov.mmo@phystech.edu>
2021-05-25 15:02:14 +0500

    fixed

--- train_and_save.py
+++ train_and_save.py
@@ -1,22 +1,24 @@
 from deeppavlov import configs, train_model
 from deeppavlov.core.common.file import read_json
+from deeppavlov import configs, train_model
+from deeppavlov.core.common.file import read_json
 import json
 import os
 
 
-def train(data='tezro_data.csv', save_path='model_config.json'):
+def train(config_path='model_config.json'):
     os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-    model_config = read_json(configs.faq.tfidf_logreg_en_faq)
-    model_config['dataset_reader']['data_path'] = data
-    model_config['dataset_reader']['data_url'] = None
+
+    model_config = read_json(config_path)
 
     model = train_model(model_config)
-    save(model_config, save_path)
+    save(model_config, config_path)
 
 
 def save(model_config, save_path):
     with open(save_path, 'w') as outfile:
         json.dump(model_config, outfile)
-        
+
+
 if __name__ == "__main__":
     train()
