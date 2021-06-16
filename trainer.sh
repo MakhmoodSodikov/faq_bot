2fffc4b7ed31a70e3457d789a4fb3aeab042b2ab
MakhmoodSodikov <sodikov.mmo@phystech.edu>
2021-06-16 04:34:36 +0300

    reqs and trainer update

--- trainer.sh
+++ trainer.sh
@@ -1,5 +1,7 @@
 sudo apt install virtualenv
-virtualenv
-python -m spacy download en_core_web_sm
+python3.6 -m venv faq_bot_venv
+source faq_bot_venv/bin/activate
 pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+python -m deeppavlov install intent_catcher
 python train_and_save.py
